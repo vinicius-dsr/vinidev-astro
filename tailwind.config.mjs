318291be20388be77e9a882f0d7024ca1d3b7e1e
Vinícius Reis <viniciusreis3105@gmail.com>
2024-11-01 11:46:36 -0300

    feat: add theme colors and theme toggle

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					light: "#679FB4", // Secondary color for light mode
+					DEFAULT: "#004F6D", // Primary color
+					dark: "#004F6D", // Same primary color for dark mode
+				},
+				secondary: {
+					light: "#004F6D", // Primary color for light mode
+					DEFAULT: "#679FB4", // Secondary color
+					dark: "#679FB4", // Same secondary color for dark mode
+				},
+				background: {
+					light: "#FFFFFF", // White background for light mode
+					dark: "#121212", // Dark background for dark mode
+				},
+				text: {
+					light: "#000000", // Black text for light mode
+					dark: "#FFFFFF", // White text for dark mode
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
