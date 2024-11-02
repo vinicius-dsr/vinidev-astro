ac22d4caea7c2cbb37be7cc24a67446eb19b05fc
Vinícius Reis <viniciusreis3105@gmail.com>
2024-11-02 12:53:44 -0300

    feat: add social links with links in navbar

---
+++ src/config/socialLinks.ts
@@ -0,0 +1,19 @@
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+export const links = [
+	{
+		icon: FaGithub,
+		name: "Github",
+		href: "https://github.com/vinicius-dsr",
+	},
+	{
+		icon: FaLinkedin,
+		name: "Linkedin",
+		href: "https://www.linkedin.com/in/viniciusreis-pgm/",
+	},
+	{
+		icon: FaInstagram,
+		name: "Instagram",
+		href: "https://www.instagram.com/vinicius_dev/",
+	},
+];
