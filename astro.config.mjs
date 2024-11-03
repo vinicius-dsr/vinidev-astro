83a58775123c8320ee5353e2ddc52f17398dde5f
Vinícius Reis <viniciusreis3105@gmail.com>
2024-11-02 23:54:30 -0300

    feat: add initial about section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+	integrations: [
+		tailwind(),
+		react(),
+		starlight({
+			title: "My Documentation",
+			plugins: [],
+		}),
+	],
 });
