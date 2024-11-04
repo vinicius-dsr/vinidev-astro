7309ef6d30b8d11dc935d790f63def735885416f
Vinícius Reis <viniciusreis3105@gmail.com>
2024-11-03 21:18:44 -0300

    feat: add service section and change folder with data information, like services, skills and social links

---
+++ src/data/services.ts
@@ -0,0 +1,26 @@
+export const services = [
+	{
+		title: "Desenvolvimento Web",
+		description:
+			"Desenvolvimento Web em Geral, Sites, Ladding Pages, Blogs etc.",
+	},
+	{
+		title: "Desenvolvimento Mobile",
+		description: "Desenvolvimento Mobile em Geral, Apps, Games etc.",
+	},
+	{
+		title: "UI/UX Design",
+		description:
+			"Interfaces para websites e aplicativos para qualquer nicho, focado em seu público e forte apelo visual.",
+	},
+	{
+		title: "E-commerce",
+		description:
+			"Criação de E-commerces, catalogação de produtos e configuração completa em platformas de marketplace para sua loja virtual.",
+	},
+	{
+		title: "Seo",
+		description:
+			"Otimização de sites para motores de busca, aumentando o tráfego orgânico e a visibilidade dos sites.",
+	},
+];
