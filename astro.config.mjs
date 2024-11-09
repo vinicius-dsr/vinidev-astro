c2277ec12b326cdc6eaffa89bf24e7eae1920d89
unknown <viniciusreis3105@gmail.com>
2024-11-09 16:15:33 -0300

    feat: disable astro devtoolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
+	devToolbar: {
+		enabled: false,
+	},
 	output: "hybrid",
 	adapter: vercel(),
 
