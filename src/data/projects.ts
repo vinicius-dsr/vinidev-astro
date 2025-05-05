2008efdfa52dfc8859e36d1d73df22722510ad82
Vinícius Reis <viniciusreis3105@gmail.com>
2025-05-05 00:26:45 -0300

    add projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,44 @@
+export const projects = [
+	{
+		title: "Lading Page de Psicologia",
+		slug: "lp-psicologia",
+		description: "",
+		image: "",
+	},
+	{
+		title: "Agenda Paragominas",
+		slug: "agenda-paragominas",
+		description: "",
+		image: "",
+	},
+	{
+		title: "Blog MDX",
+		slug: "blog-mdx",
+		description: "",
+		image: "",
+	},
+	{
+		title: "Sunclean Energia Solar",
+		slug: "",
+		description: "",
+		image: "",
+	},
+	{
+		title: "Cardápio Digital",
+		slug: "sunclean-energia-solar",
+		description: "",
+		image: "",
+	},
+	{
+		title: "Dieta AI",
+		slug: "dienta-ai",
+		description: "",
+		image: "",
+	},
+	{
+		title: "",
+		slug: "",
+		description: "",
+		image: "",
+	},
+];
