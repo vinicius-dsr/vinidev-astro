ab084511e9aee97056e07132c9b278ab98c08daa
Vinícius Reis <viniciusreis3105@gmail.com>
2025-05-07 07:17:44 -0300

    feat: add project page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,8 @@ export const projects = [
 		name: "Lading Page de Psicologia",
 		slug: "lp-psicologia",
 		imageURL: "/psicologia.avif",
-		description: "Landing Page de Psicologia",
+		description:
+			"Case criada para escritório de psicologia ou psicólogos independentes.",
 		tag: "Landing Page",
 		date: "2024",
 	},
@@ -13,7 +14,8 @@ export const projects = [
 		name: "Agenda Paragominas",
 		slug: "agenda-paragominas",
 		imageURL: "/agenda-pgm.avif",
-		description: "Agenda Paragominas",
+		description:
+			"Projetos MVP criado como um hub digital onde contem em seu banco de dados vários estabelecimentos e serviços da cidade de Paragominas.",
 		tag: "Web App",
 		date: "2024",
 	},
@@ -21,7 +23,8 @@ export const projects = [
 		name: "Blog MDX",
 		slug: "blog-mdx",
 		imageURL: "/project-blog.avif",
-		description: "Blog MDX",
+		description:
+			"Blog criado para fins didáticos, com o intuito de aprender e ensinar. Ele conta com artigos escritos em MDX, que é uma extensão do Markdown que permite incluir componentes React dentro do conteúdo.",
 		tag: "Blog",
 		date: "2024",
 	},
@@ -29,15 +32,8 @@ export const projects = [
 		name: "Reis & Reis - Advocacia",
 		slug: "reis-reis-advocacia",
 		imageURL: "/project-adv.avif",
-		description: "Reis & Reis - Advocacia",
-		tag: "Lading Page",
-		date: "2024",
-	},
-	{
-		name: "Reis & Reis - Advocacia",
-		slug: "reis-reis-advocacia",
-		imageURL: "/project-adv.avif",
-		description: "Reis & Reis - Advocacia",
+		description:
+			"Protótipo de site para escritório de advocacia. Designer feito em Figma sem versão funcional no momento",
 		tag: "Lading Page",
 		date: "2024",
 	},
@@ -45,7 +41,8 @@ export const projects = [
 		name: "Cardápio Digital",
 		slug: "cardapio-digital",
 		imageURL: "/project-cardapio.avif",
-		description: "Cardápio Digital",
+		description:
+			"Projeto de cardápio digital para restaurantes, bares e lanchonetes. Feito para fins didátivos com fotos, valores e informações fictícias.",
 		tag: "E-commerce",
 		date: "2024",
 	},
@@ -53,7 +50,8 @@ export const projects = [
 		name: "Dieta AI",
 		slug: "dieta-ai",
 		imageURL: "/dieta-ai.avif",
-		description: "Dieta AI",
+		description:
+			"Aplicativo mobile para controle de dieta geradas por inteligência artificial.",
 		tag: "App Mobile",
 		date: "2024",
 	},
