29c16d3bc9e6dfafe2b86b089ebec3c9a4f531f7
unknown <viniciusreis3105@gmail.com>
2025-07-16 21:45:00 -0300

    feat: add new project info and logic in slug page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,9 +1,12 @@
-import { desc } from "framer-motion/client";
 
 export const projects = [
 	{
 		name: "Lading Page de Psicologia",
 		slug: "lp-psicologia",
+		links: {
+			github: "https://github.com/vinicius-dsr/psicologia-lp",
+			website: "https://psicologia-lp.vercel.app/",
+		},
 		imageURL: "/psicologia.avif",
 		description:
 			"Case criada para escritório de psicologia ou psicólogos independentes.",
@@ -13,6 +16,10 @@ export const projects = [
 	{
 		name: "Agenda Paragominas",
 		slug: "agenda-paragominas",
+			links: {
+			github: "https://github.com/vinicius-dsr/agenda-paragominas",
+			website: "https://agenda-paragominas.vercel.app/",
+		},
 		imageURL: "/agenda-pgm.avif",
 		description:
 			"Projetos MVP criado como um hub digital onde contém em seu banco de dados vários estabelecimentos e serviços da cidade de Paragominas.",
@@ -22,6 +29,11 @@ export const projects = [
 	{
 		name: "Blog MDX",
 		slug: "blog-mdx",
+					links: {
+			github: "https://github.com/vinicius-dsr/mdx-blog",
+			website: "https://mdx-blog-psi.vercel.app/",
+			post: "https://www.instagram.com/p/C7O6gnGOH9Y/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+		},
 		imageURL: "/project-blog.avif",
 		description:
 			"Blog criado para fins didáticos, com o intuito de aprender e ensinar. Ele conta com artigos escritos em MDX, que é uma extensão do Markdown que permite incluir componentes React dentro do conteúdo.",
@@ -31,6 +43,9 @@ export const projects = [
 	{
 		name: "Reis & Reis - Advocacia",
 		slug: "reis-reis-advocacia",
+					links: {
+			post: "https://www.instagram.com/p/C7P1yYsRGk7/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+		},
 		imageURL: "/project-adv.avif",
 		description:
 			"Protótipo de site para escritório de advocacia. Designer feito em Figma sem versão funcional no momento",
@@ -40,6 +55,10 @@ export const projects = [
 	{
 		name: "Cardápio Digital",
 		slug: "cardapio-digital",
+					links: {
+			github: "https://github.com/vinicius-dsr/cardapio-digital",
+			website: "https://cardapio-digital-sable.vercel.app/",
+		},
 		imageURL: "/project-cardapio.avif",
 		description:
 			"Projeto de cardápio digital para restaurantes, bares e lanchonetes. Feito para fins didátivos com fotos, valores e informações fictícias.",
@@ -49,6 +68,9 @@ export const projects = [
 	{
 		name: "Dieta AI",
 		slug: "dieta-ai",
+					links: {
+			github: "https://github.com/vinicius-dsr/dietaapp-frontend",
+		},
 		imageURL: "/dieta-ai.avif",
 		description:
 			"Aplicativo mobile para controle de dieta geradas por inteligência artificial.",
