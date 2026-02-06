45e38a7398b3965ea7c0ceea5d4db9e54d716042
Vinícius Reis <viniciusreis3105@gmail.com>
2026-02-06 20:15:49 -0300

    feat: change social links and add new whastsapp button content

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,24 +1,24 @@
 import { FaGithub, FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 
 export const links = [
-	{
-		icon: FaGithub,
-		name: "Github",
-		href: "https://github.com/vinicius-dsr",
-	},
-	{
-		icon: FaLinkedin,
-		name: "Linkedin",
-		href: "https://www.linkedin.com/in/viniciusreis-pgm/",
-	},
-	{
-		icon: FaInstagram,
-		name: "Instagram",
-		href: "https://www.instagram.com/vinicius_dev/",
-	},
-	{
-		icon: FaWhatsapp,
-		name: "WhatsApp",
-		href: "https://wa.me/5591992761377",
-	},
+  {
+    icon: FaGithub,
+    name: "Github",
+    href: "https://github.com/vinicius-dsr",
+  },
+  {
+    icon: FaLinkedin,
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/viniciusreis-pgm/",
+  },
+  {
+    icon: FaInstagram,
+    name: "Instagram",
+    href: "https://www.instagram.com/vinicius_dev/",
+  },
+  {
+    icon: FaWhatsapp,
+    name: "WhatsApp",
+    href: "https://wa.me/5591991866344?text=Olá,%20vim%20pelo%20site",
+  },
 ];
